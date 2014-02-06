99ea655462d84f6e05bedbb12f9937fcec6a843b
piotrekbass <piotrek.koscielniak@gmail.com>
2014-02-06 21:16:36 +0100

    clearTimeout

--- timersAndScheduling/usingSetTimeout.js
+++ timersAndScheduling/usingSetTimeout.js
@@ -3,4 +3,13 @@ setTimeout(function (foo, bar) {
 }, 1000, "foo", "bar");
 
 
+var timeoutId = setTimeout(function () {
+    console.log("It doesn't happen");
+}, 1000);
+clearTimeout(timeoutId);
+
+
+
+
+
 
