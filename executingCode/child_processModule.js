8b57cf7ff98ac84a518a74bb2642b94da0a1af80
piotrekbass <piotrek.koscielniak@gmail.com>
2014-02-15 13:35:39 +0100

    execFile

--- executingCode/child_processModule.js
+++ executingCode/child_processModule.js
@@ -11,3 +11,14 @@ cp.exec("ls -l /", {
         console.log(stdout);
     }
 });
+
+
+cp.execFile("ls", function (error, stdout, stderr) {
+    if (error) {
+        console.error(error.toString());
+    } else if (stderr !== "") {
+        console.error(stderr);
+    } else {
+        console.log(stdout);
+    }
+})
