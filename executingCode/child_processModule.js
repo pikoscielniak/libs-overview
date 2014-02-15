de88dfdcdaa5b8ebafd9701b35edeed4949dae85
piotrekbass <piotrek.koscielniak@gmail.com>
2014-02-15 13:34:01 +0100

    exec with options

--- executingCode/child_processModule.js
+++ executingCode/child_processModule.js
@@ -1,6 +1,8 @@
 var cp = require('child_process');
 
-cp.exec("ls -l /", function (error, stdout, stderr) {
+cp.exec("ls -l /", {
+    cwd: '/'
+}, function (error, stdout, stderr) {
     if (error) {
         console.error(error.toString());
     } else if (stderr !== "") {
